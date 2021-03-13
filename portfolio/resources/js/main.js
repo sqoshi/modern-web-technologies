8dabc44ec303a5e7a8259e6f3b93831e78f9c351
piotr <piotrpopis98@icloud.com>
2021-03-13 17:13:21 +0100

    almost finished first page- mobile

--- portfolio/resources/js/main.js
+++ portfolio/resources/js/main.js
@@ -8,3 +8,23 @@ function toggle_menu() {
     content.style.maxHeight = content.scrollHeight + "px";
   }
 }
+
+
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("img").src = images[x];
+}
+
+function displayPreviousImage() {
+    x = (x <= 0) ? images.length - 1 : x - 1;
+    document.getElementById("img").src = images[x];
+}
+
+function startTimer() {
+    setInterval(displayNextImage, 6000);
+}
+
+var images = [], x = -1;
+images[0] = "https://raw.githubusercontent.com/sqoshi/global-warming-investigator/master/static/images/world_map.png";
+images[1] = "https://raw.githubusercontent.com/sqoshi/neural-network-designer/master/static/images/results_example.png";
+images[2] = "https://raw.githubusercontent.com/sqoshi/zalando-shopping-bot/master/features/screenshots/panelbot.png";
