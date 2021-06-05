79108ab193799e5bd3233d8c9a47dded3c946fed
piotr <piotrpopis98@icloud.com>
2021-06-05 23:36:39 +0200

    removed old

--- mathematical_content_formator/math_formattor/README.md
+++
@@ -1,4 +0,0 @@
-# math_formattor
-
-## Launch
-`flutter run --no-sound-null-safety -d chrome`
--- mathematical_content_formator/math_formattor/lib/constant.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xFFFFC200);
-const kTextColor = Color(0xFF1e1e1e);
-const kDarkColor = Color(0xFF007acc);
-
-const kMinWidthOfLargeScreen = 1200;
